dc67108d9567eadd5b1fcb6ca2617a3493255a69
OlivierLem <lemort.olivier.pro@gmail.com>
2023-03-07 08:59:08 +0100

    first commit

---
+++ dist/topbar.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************************!*\
+  !*** ./src/assets/javascript/topbar.js ***!
+  \*****************************************/
+console.log('topbar');
+/******/ })()
+;
+//# sourceMappingURL=topbar.bundle.js.map
